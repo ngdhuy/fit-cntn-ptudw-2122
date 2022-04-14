09c37db22b421a22bbfbf520d4dbfb796cd3ac6f
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-04-14 11:10:47 +0700

    feat(js): variable

---
+++ lab01-es/demo01.js
@@ -0,0 +1,20 @@
+console.clear();
+
+const PI = 3.14; 
+console.log(PI);
+
+var a = 10; 
+console.log(a);
+
+if(a % 2 == 0) {
+  a = 8; 
+  console.log(a); 
+  
+  let b = "hello world";
+  console.log(b); 
+
+  var c = 'Bien C ne';
+}
+
+// console.log(b);
+console.log(c);
