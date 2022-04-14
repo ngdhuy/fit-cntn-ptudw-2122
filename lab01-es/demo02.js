6cfaf7925884985c039c2894da5dc64c32fefca6
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2022-04-14 11:17:09 +0700

    feat(js): operator ===

---
+++ lab01-es/demo02.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.clear();
+
+let a = 2;
+let b = 2.0; 
+let c = '2';
+
+console.log(a == b);
+console.log(a == c);
+console.log(b == c);
+
+console.log('-----------');
+console.log(a === b);
+console.log(a === c);
+console.log(b === c);
